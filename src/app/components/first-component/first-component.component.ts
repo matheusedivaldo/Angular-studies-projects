d32bc7292a66fe84be9d44f3dbc5b061d2d1ce8e
matheusedivado <ms1501085@gmail.com>
2024-06-16 19:24:33 -0300

    envia projeto de curso angular

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css'
+})
+export class FirstComponentComponent {
+
+  name: string = "Matheus";
+  age: number = 18;
+  job = "Junior programmer";
+  hobbies = ["Ver Filmes", "Treinar"];
+  car = {
+    name: 'Uno',
+    year: 2015
+  }
+}
